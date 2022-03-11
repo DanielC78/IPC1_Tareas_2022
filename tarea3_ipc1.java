778d4d3683c1c0a4ea07beb84254b11b0664980a
DanielC78 <3024465830102@ingenieria.usac.edu.gt>
2022-03-10 20:23:21 -0600

    crearArreglo

--- tarea3_ipc1.java
+++ tarea3_ipc1.java
@@ -1,10 +1,9 @@
 import java.util.*;
 
-import javafx.scene.control.SplitPane.Divider;
-
 public class tarea3_ipc1 {
 
 
+    private static int [] numeros = {7, 5, 8, 11, 2, 3, 5, 7}; 
     public static void main(String[] args) {
         
     }
