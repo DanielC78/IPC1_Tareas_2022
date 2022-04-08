25506453d1042adb774d7c987e9c233c3ac6bd38
DanielC78 <3024465830102@ingenieria.usac.edu.gt>
2022-04-08 17:51:11 -0600

    prueba

--- tarea3_ipc1.java
+++ tarea3_ipc1.java
@@ -2,71 +2,73 @@ import java.util.*;
 
 public class tarea3_ipc1 {
 
+    // Aplicando Quicksort
 
-    //Aplicando Quicksort
-
-    public tarea3_ipc1(){
+    public tarea3_ipc1() {
 
     }
 
-    public static int [] numeros = {7, 5, 8, 11, 2, 3, 5, 7}; 
+    public static int[] numeros = { 7, 5, 8, 11, 2, 3, 5, 7 };
 
-
-    public int[] quicksort(int[] arreglo){
-        return quicksort(arreglo, 0, numeros.length-1);
+    public int[] quicksort(int[] arreglo) {
+        return quicksort(arreglo, 0, numeros.length - 1);
     }
 
-    public int[] quicksort(int [] arreglo, int izq, int der){
+    public int[] quicksort(int[] arreglo, int izq, int der) {
 
-        if(izq >= der)
-        return arreglo;
+        if (izq >= der)
+            return arreglo;
         int i = izq, d = der;
-        if(izq != der){
+        if (izq != der) {
             int pivote;
             int aux;
             pivote = izq;
-            while(izq!= der)
-            { imprimirArreglo(arreglo);
-                while(arreglo[der] >= arreglo[pivote] && izq < der)
-                der--;
-            while(arreglo[izq] < arreglo[pivote] && izq < der)
-                izq++;
-            
-                if(der != izq){
+            while (izq != der) {
+                imprimirArreglo(arreglo);
+                while (arreglo[der] >= arreglo[pivote] && izq < der)
+                    der--;
+                while (arreglo[izq] < arreglo[pivote] && izq < der)
+                    izq++;
+
+                if (der != izq) {
                     aux = arreglo[der];
                     arreglo[der] = arreglo[izq];
                     numeros[izq] = aux;
                 }
 
-
-                if(izq == der){
-                    quicksort(arreglo, i, izq-1);
-                    quicksort(arreglo,izq+1,d);
+                if (izq == der) {
+                    quicksort(arreglo, i, izq - 1);
+                    quicksort(arreglo, izq + 1, d);
                 }
-            } 
-        }else{
-                return arreglo;
             }
+        } else {
             return arreglo;
+        }
+        return arreglo;
     }
 
-    public void imprimirArreglo(int arreglo[]){
+    public void imprimirArreglo(int arreglo[]) {
         String imp = "";
         for (int i = 0; i < arreglo.length; i++) {
-                if(i!= arreglo.length)
-                imp += arreglo[i]+",";
-                else
-                imp+= arreglo[i]+"";
+            if (i != arreglo.length)
+                imp += arreglo[i] + ",";
+            else
+                imp += arreglo[i] + "";
         }
         System.out.println(imp);
     }
+
+    private int sumar() {
+        return 1 + 1;
+    }
+
     public static void main(String[] args) {
-        
-        tarea3_ipc1 ob =  new tarea3_ipc1();
+
+        tarea3_ipc1 ob = new tarea3_ipc1();
         ob.quicksort(numeros);
 
         int i = 0;
-        while(i < numeros.length){
+        while (i < numeros.length) {
             System.out.println(numeros[i]);
             i++;
         }
